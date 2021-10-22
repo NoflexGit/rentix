450fcb9ef34448ff25922edd453a988a4cbbb238
NoflexGit <untroman@mail.ru>
2021-10-22 19:50:08 +0300

    Images added

---
+++ src/store/AuthSlice/index.ts
@@ -0,0 +1,25 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+interface IAuthState {
+  isAuthenticated: boolean;
+}
+
+const initialState: IAuthState = {
+  isAuthenticated: true,
+};
+
+const authSlice = createSlice({
+  name: 'auth',
+  initialState,
+  reducers: {
+    login(state) {
+      state.isAuthenticated = true;
+    },
+    logout(state) {
+      state.isAuthenticated = false;
+    },
+  },
+});
+
+export const { login, logout } = authSlice.actions;
+export default authSlice.reducer;
