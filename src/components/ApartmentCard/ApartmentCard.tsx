e82aa050c52f3479951c4f4866508cdf869a6e33
NoflexGit <untroman@mail.ru>
2021-10-25 23:19:42 +0300

    Apartment card added

---
+++ src/components/ApartmentCard/ApartmentCard.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import styles from './ApartmentCard.module.scss';
+import apartment1 from '@/assets/images/apartment-1.jpeg';
+
+const ApartmentCard = (): JSX.Element => {
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.image}>
+        <img src={apartment1} alt="Anemoonstraat 11MP" />
+      </div>
+      <div className={styles.info}>
+        <div className={styles.address}>
+          <p>193 May Street</p>
+          <span>New Port, 893 HS London</span>
+        </div>
+        <div className={styles.dates}>
+          <p>Rented from: February 25, 2020</p>
+          <span>Next payment: May 25 2022</span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ApartmentCard;
--- src/pages/Apartments/Apartments.tsx
+++ src/pages/Apartments/Apartments.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Row, Col } from '@/components/Grid';
-import apartment1 from '@/assets/images/apartment-1.jpeg';
 import useDocumentTitle from '@/hooks/useDocumentTitle';
+import ApartmentCard from '@/components/ApartmentCard/ApartmentCard';
 import styles from './Apartments.module.scss';
 
 const ApartmentsPage = (): JSX.Element => {
@@ -9,21 +9,7 @@ const ApartmentsPage = (): JSX.Element => {
   return (
     <Row tag="section">
       <Col width="4">
-        <section className={styles.apartmentsList}>
-          <div className={styles.apartmentsListItem}>
-            <div className={styles.apartmentsListItemImg}>
-              <img src={apartment1} alt="Anemoonstraat 11MP" />
-            </div>
-            <div className={styles.apartmentsListItemAddress}>
-              <p>193 May Street</p>
-              <span>New Port, 893 HS London</span>
-            </div>
-            <div className={styles.apartmentsListItemDates}>
-              <div>Rented from February 25, 2020</div>
-              <div>Next payment: 25 May 2022</div>
-            </div>
-          </div>
-        </section>
+        <ApartmentCard />
       </Col>
     </Row>
   );
