907dc3502f47c69413d332b95ff00b2eae9dcb98
NoflexGit <untroman@mail.ru>
2021-10-26 22:14:55 +0300

    Price label added

--- src/components/ApartmentCard/ApartmentCard.tsx
+++ src/components/ApartmentCard/ApartmentCard.tsx
@@ -6,6 +6,7 @@ const ApartmentCard = (): JSX.Element => {
   return (
     <div className={styles.wrapper}>
       <div className={styles.image}>
+        <div className={styles.price}>1200$</div>
         <img src={apartment1} alt="Anemoonstraat 11MP" />
       </div>
       <div className={styles.info}>
@@ -14,7 +15,7 @@ const ApartmentCard = (): JSX.Element => {
           <span>New Port, 893 HS London</span>
         </div>
         <div className={styles.dates}>
-          <p>Rented from: February 25, 2020</p>
+          <p>Rented from: Feb 25, 2020</p>
           <span>Next payment: May 25 2022</span>
         </div>
       </div>
